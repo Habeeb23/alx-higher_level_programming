108b643983f3ab979ca0f4fe01300501230c62de
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-10 03:42:55 +0800

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-
-
-def print_list_integer(my_list=[]):
-
-        for elem in my_list:
-
-                    print("{:d}".format(elem))
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    ret = my_list[0]
+    for num in my_list:
+        if num > ret:
+            ret = num
+    return ret
