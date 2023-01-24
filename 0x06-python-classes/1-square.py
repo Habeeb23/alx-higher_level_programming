12d020be4791a44f89e7ad126a40cdb292915745
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-25 00:02:46 +0800

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,7 @@
 class Square:
     """
     Defines a Square and its basic properties
+    
     >>> square_1 = Square()
     >>> square_2 = Square(7)
     """
@@ -12,6 +13,7 @@ class Square:
     def __init__(self, size: int) -> None:
         """
         Innitialize the size of the square
+        
         :param size: int size of square
         """
         self.__size = size
