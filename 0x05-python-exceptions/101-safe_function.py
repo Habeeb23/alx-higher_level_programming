af25a477011b8e5b65de34b34e5e98f1b1207a24
Habeeb23 <jimohhabeeb23@gmail.com>
2023-01-24 06:58:21 -0800

    103-python.c

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,23 @@
+afe_print_list(my_list=[], x=0):
+
+        counter = 0
+
+            for i in range(x):
+
+                        try:
+
+                                        print("{}".format(my_list[i]), end="")
+
+                                                except IndexError:
+
+                                                                break
+
+                                                                    else:
+
+                                                                                    counter += 1
+
+                                                                                        print()
+
+                                                                                            return counter
+
+
