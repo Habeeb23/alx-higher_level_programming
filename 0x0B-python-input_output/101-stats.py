53013a40ad495b50ecb2d2bedeea63397eca8d5d
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-02-09 07:07:37 +0800

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,18 +1,57 @@
-
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-"""Defines a text file-reading function."""
 
+if __name__ == "__main__":
+    import sys
 
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
+            line = line.split()
 
-def read_file(filename=""):
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-        """Print the contents of a UTF8 text file to stdout."""
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
 
-            with open(filename, encoding="utf-8") as f:
+        print_stats(size, status_codes)
 
-                        print(f.read(), end="")
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
