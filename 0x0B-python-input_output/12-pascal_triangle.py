609064b543c06eb24f3d109dc494e205ff15d783
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-02-09 07:08:55 +0800

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
+"""Defines a Pascal's Triangle function."""
 
 
-"""Defines a text file-reading function."""
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
 
-
-
-
-
-def read_file(filename=""):
-
-        """Print the contents of a UTF8 text file to stdout."""
-
-            with open(filename, encoding="utf-8") as f:
-
-                        print(f.read(), end="")
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
