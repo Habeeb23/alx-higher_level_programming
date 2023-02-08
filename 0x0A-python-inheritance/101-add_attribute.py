cdc769d1793e8253d4c8d3803be58b01bae0f8e0
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-02-09 06:08:59 +0800

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 
-''' Module: 1-my_list
-
-'''
+"""Defines a function that adds attributes to objects."""
 
 
-
-
-
-class MyList(list):
-
-        ''' Represents a MyList
-
-            '''
-
-
-
-                def print_sorted(self):
-
-                            '''
-
-                                    prints the list, but sorted
-
-                                            '''
-
-                                                    print(sorted(self))
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
